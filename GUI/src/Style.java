4a03b7e6bfe7d5e4dbd0380c97958945651004e3
Kkkaterina <eicolonel@icloud.com>
2025-11-29 23:47:35 +0300

    Домашняя работа GUI

---
+++ GUI/src/Style.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+public class Style {
+
+    public static final Color background = new Color(230, 220, 255);
+    public static final Color message = new Color(180, 100, 255);
+
+    public static final int radius = 20;
+
+}
